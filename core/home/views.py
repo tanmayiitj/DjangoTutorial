bcb8e32a77ce3a378c0db93c9542625a9e149113
Tanmay <tanmay.1@iitj.ac.in>
2024-01-23 00:18:45 +0530

    second commit

--- core/home/views.py
+++ core/home/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-def home(request):
-    return render(request, "index.html")
-
 def success_page(request):
     return HttpResponse("Hey again fucker!!")
+def home(request):
+    people = [
+        {'name':'Tanmay', 'age':22},
+        {'name':'Priyanshu', 'age':22},
+        {'name':'Shahzen', 'age':23},
+        {'name':'Rohit', 'age':20},
+        {'name':'Piyush', 'age':23},
+        {'name':'Pulkit', 'age':22},
+    ]
+
+    return render(request,"index.html",context={'peoples':people})
